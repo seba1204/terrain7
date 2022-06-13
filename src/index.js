88fe40213d349fb6f2bf411dd1df8e51974fceab
Sébastien PONT <seba12.04@orange.fr>
2022-06-13 11:29:34 +0200

    feat: :sparkles: add lights

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-camera.position.z = 1;
-
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const NB_VERTICES = 10;
-
-const wireframe = true;
-const widthSegments = NB_VERTICES;
-const heightSegments = NB_VERTICES;
-
-const geometry = new THREE.PlaneGeometry(1, 1, widthSegments, heightSegments);
-const material = new THREE.MeshBasicMaterial({ color: 0x049ef4, side: THREE.DoubleSide, wireframe });
-const plane = new THREE.Mesh(geometry, material);
-scene.add(plane);
-
-// const light = new THREE.AmbientLight(0x404040); // soft white light
-// scene.add(light);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
 document.body.appendChild(renderer.domElement);
 
-// animation
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-function animation(time) {
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
 
-    plane.rotation.x = time / 2000;
-    plane.rotation.y = time / 1000;
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
+};
 
-}
-
-// animate();
+animate();
