2f8b0cc26a9fa1ecebfbfe4867fc66aac8e8c904
Sébastien PONT <seba12.04@orange.fr>
2022-06-13 11:44:37 +0200

    feat: :sparkles: try to add controls

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
+// import orbit controls
+// import { OrbitControls } from '@/node_modules/three/examples/jsm/controls/OrbitControls';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const loader = new THREE.TextureLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const materials = [
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-1.jpg') }),
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-2.jpg') }),
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-3.jpg') }),
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-4.jpg') }),
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-5.jpg') }),
+    new THREE.MeshBasicMaterial({ map: loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/flower-6.jpg') }),
+];
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 scene.add(directionalLight);
 
-camera.position.z = 5;
+camera.position.set(0, 0, 5);
+// controls.update();
 
 function animate() {
     requestAnimationFrame(animate);
+
+    // controls.update();
+
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
