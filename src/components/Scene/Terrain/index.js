8b7c87de7d7fd4d8963a90726bf9b771d2f8b54a
Sébastien PONT <sebastien.pont@etu.toulouse-inp.fr>
2022-06-16 09:41:36 +0200

    feat: :sparkles: port all three.js to r3f

---
+++ src/components/Scene/Terrain/index.js
@@ -0,0 +1,51 @@
+import React, { useRef } from "react";
+import { useFrame } from "react-three-fiber";
+
+import { noise } from "./perlin";
+
+const Terrain = () => {
+    const mesh = useRef();
+    const didUpdate = (ref) => {
+        console.log(ref);
+        noise.seed(Math.random());
+        let pos = ref.geometry.getAttribute("position");
+        let pa = pos.array;
+        const hVerts = ref.geometry.parameters.heightSegments + 1;
+        const wVerts = ref.geometry.parameters.widthSegments + 1;
+        for (let j = 0; j < hVerts; j++) {
+            for (let i = 0; i < wVerts; i++) {
+                const ex = 1.1;
+                pa[3 * (j * wVerts + i) + 2] =
+                    (noise.simplex2(i / 100, j / 100) +
+                        noise.simplex2((i + 200) / 50, j / 50) * Math.pow(ex, 1) +
+                        noise.simplex2((i + 400) / 25, j / 25) * Math.pow(ex, 2) +
+                        noise.simplex2((i + 600) / 12.5, j / 12.5) * Math.pow(ex, 3) +
+                        +(noise.simplex2((i + 800) / 6.25, j / 6.25) * Math.pow(ex, 4))) /
+                    2;
+            }
+        }
+
+        pos.needsUpdate = true;
+    };
+
+    // Raf loop
+    useFrame(() => {
+        mesh.current.rotation.z += 0.001;
+    });
+
+    return (
+        <mesh ref={mesh} rotation={[-Math.PI / 3, 0, Math.PI / 10]} onUpdate={didUpdate} >
+            <planeBufferGeometry attach="geometry" args={[25, 25, 75, 75]} />
+            <meshPhongMaterial
+                attach="material"
+                color={0x3498db}
+                specular={0x3498db}
+                shininess={3}
+                flatShading
+            />
+        </mesh>
+    );
+};
+
+export default Terrain;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable  @typescript-eslint/no-non-null-assertion */
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     App()
